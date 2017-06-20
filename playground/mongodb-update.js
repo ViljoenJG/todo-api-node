16568fafd25265772d2cc7ca138316eb0382cc61
Kobus Viljoen <kobusv@gmail.com>
2017-06-20 17:23:46 +0200

    added update examples

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,6 +6,47 @@ MongoClient.connect('mongodb://localhost:27017/todo-app', (err, db) => {
     }
 
     // findOneAndUpdate
+    // let filter = {
+    //     _id: new ObjectID('59404ba0d637f9f6cefae9fa')
+    // };
+    //
+    // let action = {
+    //     $set: {
+    //         completed: true
+    //     }
+    // };
+    //
+    // let options = {
+    //     returnOriginal: false
+    // };
+    //
+    // db.collection('Todos')
+    //     .findOneAndUpdate(filter, action, options)
+    //     .then(
+    //         result => console.log('Result: %o', result),
+    //         err => console.error('Error: %o', err)
+    //     );
+
+    // Update User
+    let filter = {
+        _id: new ObjectID('59493beca566210d6d423977')
+    };
+
+    let updateAction = {
+        $inc: {
+            age: -1
+        },
+        $set: {
+            name: 'Kobus Viljoen'
+        }
+    };
+
+    db.collection('Users')
+        .findOneAndUpdate(filter, updateAction, {returnOriginal: false})
+        .then(
+            resp => console.log(JSON.stringify(resp.value, null, 2)),
+            err => console.error(err)
+        );
 
     db.close();
 });
