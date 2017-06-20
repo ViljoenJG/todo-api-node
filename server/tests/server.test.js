4249142a6e4754342b5b334db180b749cbc54137
Kobus Viljoen <kobusv@gmail.com>
2017-06-20 21:57:42 +0200

    added dummy data before tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -20,7 +30,7 @@ describe('POST /todos', () => {
       .end((err, res) => {
         if (err) return done(err);
 
-        Todo.find()
+        Todo.find({text})
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -38,7 +48,7 @@ describe('POST /todos', () => {
 
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done()
           })
           .catch(err => done(err))
