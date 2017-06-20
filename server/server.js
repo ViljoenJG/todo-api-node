1c1860ee9a328ba16e2735c2d912e50d8429bcd7
Kobus Viljoen <kobusv@gmail.com>
2017-06-20 20:27:53 +0200

    added mongoose models for Todo and User

--- server/server.js
+++ server/server.js
@@ -5,24 +5,47 @@ mongoose.connect('mongodb://localhost:27017/todo-app');
 
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-let newTodo = new Todo({
-    text: 'Study some NodeJS',
-    completed: true,
-    completedAt: new Date().getTime()
-});
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+})
 
-newTodo.save()
-    .then(
-        doc => console.log('Saved Todo: ', doc),
-        err => console.error(err)
-    );
+// let newTodo = new Todo({
+//     text: '  Another new todo  '
+// });
+//
+// newTodo.save()
+//     .then(
+//         doc => console.log('Saved Todo: ', doc),
+//         err => console.error(err)
+//     );
+
+
+// let user = new User({
+//   email: 'kobus@gmail.com'
+// })
+//
+// user.save()
+//   .then(
+//     doc => console.log(doc),
+//     err => console.log(err)
+//   );
