e25df92be0746acd416c436f5be35bd890ba0eb8
Kobus Viljoen <kobusv@gmail.com>
2017-06-26 22:06:03 +0200

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
@@ -35,10 +36,29 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find()
-    .then((todos) => {
-      res.send({ todos })
+    .then((data) => {
+      res.send({ data })
     }, (err) => {
       res.status(400).send(err)
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!id || !ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((data) => {
+      if (!data) {
+        return res.status(404).send();
+      }
+
+      res.send({ data });
+    }, (err) => {
+      res.status(400).send()
     })
 });
 
