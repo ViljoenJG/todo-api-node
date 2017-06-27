30a5f3f183995e8a6f0ddcd806dc5f4a3f78d1cd
Kobus Viljoen <kobusv@gmail.com>
2017-06-28 00:29:00 +0200

    start test on different port

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,6 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/todo-app'
 } else if (env === 'test') {
-  process.env.PORT = 3000;
+  process.env.PORT = 3001;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/todo-app-test'
 }
