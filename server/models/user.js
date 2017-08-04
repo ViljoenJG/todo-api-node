a19fcdddc608e44573c16c9898fdc72007934f18
Kobus Viljoen <kobusv@gmail.com>
2017-08-04 22:18:09 +0200

    hashing password before storing in DB

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
   email: {
@@ -64,6 +65,19 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function(next) {
+  let user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.hash(user.password, 10, (err, hash) => {
+      user.password = hash;
+      next()
+    })
+  } else {
+    next()
+  }
+})
 
 let User = mongoose.model('User', UserSchema);
 
