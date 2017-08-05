10ee6aefbd10c1cba60fd767397137ba6ec8b182
Kobus Viljoen <kobusv@gmail.com>
2017-08-05 11:11:20 +0200

    added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -229,9 +229,9 @@ describe('POST /users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        })
-      })
-  })
+        }).catch(e => done(e));
+      });
+  });
 
   it('should return validation error if email is invalid', (done) => {
     request(app)
@@ -270,5 +270,52 @@ describe('POST /users', () => {
       })
       .expect(400)
       .end(done)
+  })
+});
+
+describe('POST /users/login', () => {
+  it('should log in user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) { return done(err) }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch(e => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'wrongPassword'
+      })
+      .expect(401)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) { return done(err) }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe[0];
+          done();
+        }).catch(e => done(e));
+      })
   })
 })
